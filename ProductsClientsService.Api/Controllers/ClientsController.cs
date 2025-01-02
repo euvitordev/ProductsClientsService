8152780b7dbdce075633a19b1554701d62fdfb3f
euvitordev <dev.vitorlucas@gmail.com>
2025-01-01 21:13:00 -0300

    new: add client controller

---
+++ ProductsClientsService.Api/Controllers/ClientsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsClientsService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}  
